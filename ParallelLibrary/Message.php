29f688169d5afcbb13ff112b7f34a759c9bbbe28
Michael Vostrikov <michael.vostrikov@gmail.com>
2015-11-19 23:00:22 +0500

    PHPDoc - Message

--- ParallelLibrary/Message.php
+++ ParallelLibrary/Message.php
@@ -4,12 +4,26 @@ namespace ParallelLibrary;
 
 use ParallelLibrary\interfaces\IMessage;
 
-
+/**
+ * Represents message used for communication between processes
+ */
 class Message implements IMessage
 {
+    /**
+     * @var string message type
+     */
     public $type;
+
+    /**
+     * @var string message data
+     */
     public $data;
 
+
+    /**
+     * @param string message type
+     * @param string message data
+     */
     public function __construct($type, $data = null)
     {
         $this->type = $type;
--- ParallelLibrary/interfaces/IMessage.php
+++ ParallelLibrary/interfaces/IMessage.php
@@ -2,6 +2,9 @@
 
 namespace ParallelLibrary\interfaces;
 
+/**
+ * Interface that should be implemented by classes who represents message used for communication between processes
+ */
 interface IMessage
 {
     // nothing here
